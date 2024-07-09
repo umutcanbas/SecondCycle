7a646caf143ba54843d12c680fb89a62269f0b6a
umutcanbas <umutbas57@gmail.com>
2024-07-09 21:38:10 +0300

    Map page

--- src/screens/Map/Map.js
+++ src/screens/Map/Map.js
@@ -1,10 +1,14 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import ProductCard from '../../components/Products/ProductsCard'
+
+
 
 const Map = () => {
   return (
     <View>
       <Text>Map</Text>
+      <ProductCard/>
     </View>
   )
 }

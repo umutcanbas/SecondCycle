6a4e916b36c518d539d54464c58e4e3101b948b2
umutcanbas <umutbas57@gmail.com>
2024-07-09 21:35:53 +0300

    Add product and  modal

---
+++ src/components/Modal/AddProductModal.js
@@ -0,0 +1,50 @@
+import {View, Text, TextInput, Dimensions, StyleSheet} from 'react-native';
+import React from 'react';
+import Modal from 'react-native-modal';
+import AddProducts from '../Products/AddProducts';
+
+const deviceSize = Dimensions.get('window');
+
+const ContentInputModal = ({visible, onClose, onSend}) => {
+  return (
+    <Modal
+      style={styles.modal}
+      isVisible={visible}
+      swipeDirection="down"
+      onSwipeComplete={onClose}
+      onBackdropPress={onClose}
+      onBackButtonPress={onClose}>
+      <View style={styles.container}>
+        <View style={styles.line} />
+        <View style={styles.input_container}>
+          <AddProducts />
+        </View>
+      </View>
+    </Modal>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'white',
+    padding: 15,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    height: deviceSize.height / 2.1,
+  },
+  line: {
+    width: 80,
+    height: 8,
+    backgroundColor: '#e0e0e0',
+    borderRadius: 10,
+    alignSelf: 'center',
+    marginBottom: 5,
+  },
+  input_container: {flex: 1},
+  modal: {
+    justifyContent: 'flex-end',
+    margin: 0,
+  },
+});
+
+export default ContentInputModal;
---
+++ src/components/Products/AddProducts.js
@@ -0,0 +1,108 @@
+import React, {useState} from 'react';
+
+import {View, TextInput, Button, StyleSheet} from 'react-native';
+
+import database from '@react-native-firebase/database';
+
+const AddProducts = () => {
+  const [productName, setProductName] = useState('');
+  const [username, setUsername] = useState('');
+  const [description, setDescription] = useState('');
+  const [price, setPrice] = useState('');
+  const [location, setLocation] = useState('');
+  const [imageUrl, setImageUrl] = useState('');
+
+  const AddProduct = () => {
+    if (productName && username && description && price && imageUrl) {
+      const newProduct = {
+        name: productName,
+        username,
+        description,
+        price,
+        location,
+        imageUrl,
+      };
+
+      database()
+        .ref('/products')
+        .push(newProduct)
+        .then(() => {
+          console.log('Product added!');
+          setProductName('');
+          setUsername('');
+          setDescription('');
+          setLocation('');
+          setPrice('');
+          setImageUrl('');
+        })
+        .catch(error => {
+          console.error('Error adding product: ', error);
+        });
+    } else {
+      console.error('Please fill in all fields.');
+    }
+  };
+
+  return (
+    <View contentContainerStyle={styles.container}>
+      <TextInput
+        placeholder="Product Name"
+        value={productName}
+        onChangeText={setProductName}
+        style={styles.input}
+      />
+      <TextInput
+        placeholder="Username"
+        value={username}
+        onChangeText={setUsername}
+        style={styles.input}
+      />
+      <TextInput
+        placeholder="Description"
+        value={description}
+        onChangeText={setDescription}
+        style={styles.input}
+      />
+      <TextInput
+        placeholder="Price"
+        value={price}
+        onChangeText={setPrice}
+        keyboardType="numeric"
+        style={styles.input}
+      />
+      <TextInput
+        placeholder="Location"
+        value={location}
+        onChangeText={setLocation}
+        style={styles.input}
+      />
+      <TextInput
+        placeholder="Image URL"
+        value={imageUrl}
+        onChangeText={setImageUrl}
+        style={styles.input}
+      />
+      <View style={styles.buttonContainer}>
+        <Button title="Add Product" onPress={AddProduct} />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexGrow: 1,
+    padding: 20,
+    backgroundColor: '#fff',
+  },
+  input: {
+    borderBottomWidth: 1,
+    marginBottom: 10,
+    padding: 8,
+  },
+  buttonContainer: {
+    marginVertical: 20,
+  },
+});
+
+export default AddProducts;

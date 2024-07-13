d2846e39b8a7e50d5d167e2ab7ec78f2ce122b16
umutcanbas <umutbas57@gmail.com>
2024-07-13 23:01:24 +0300

    Address page added

--- src/navigation/WithOutTab.js
+++ src/navigation/WithOutTab.js
@@ -1,10 +1,10 @@
-import {View, Text} from 'react-native';
 import React from 'react';
 
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import routes from './routes';
 
 import ProductDetail from '../screens/Home/ProductDetail';
+import Address from '../screens/Profile/Address';
 
 const Stack = createNativeStackNavigator();
 
@@ -15,6 +15,7 @@ const WithOutTab = () => {
         headerShown: false,
       }}>
       <Stack.Screen name={routes.PRODUCT_DETAIL} component={ProductDetail} />
+      <Stack.Screen name={routes.ADDRESS} component={Address} />
     </Stack.Navigator>
   );
 };

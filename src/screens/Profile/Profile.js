07923ebabfb0af4e3c37ca4d1390a57c8071e4c5
umutcanbas <umutbas57@gmail.com>
2024-07-13 23:03:06 +0300

    Go address and user ınfo

--- src/screens/Profile/Profile.js
+++ src/screens/Profile/Profile.js
@@ -1,13 +1,19 @@
 import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import AsyncStorage from '@react-native-async-storage/async-storage';
+
+import auth from '@react-native-firebase/auth';
+import database from '@react-native-firebase/database';
 
 import Button from '../../components/Button/Button';
 
 import routes from '../../navigation/routes';
 
 const Profile = ({navigation}) => {
+  const [userId, setUserId] = useState(null);
+  const [userName, setUserName] = useState('');
+
   const logOut = async () => {
     try {
       await AsyncStorage.removeItem('isLogged');
@@ -18,9 +24,40 @@ const Profile = ({navigation}) => {
     }
   };
 
+  const goAddress = () => {
+    navigation.navigate(routes.WITH_OUT_TAB, {
+      screen: routes.ADDRESS,
+    });
+  };
+
+  useEffect(() => {
+    const user = auth().currentUser;
+    if (user) {
+      setUserId(user.uid);
+      database()
+        .ref(`/users/${user.uid}/username`)
+        .once('value')
+        .then(snapshot => {
+          if (snapshot.exists()) {
+            setUserName(snapshot.val());
+          } else {
+            setUserName('No username found');
+          }
+        })
+        .catch(error => {
+          console.error('Failed to fetch username:', error);
+        });
+    }
+  }, []);
+
   return (
     <SafeAreaView style={styles.container}>
       <Text>Profile</Text>
+      <Button title="Address" onPress={() => goAddress()} />
+
+
+      <Text>Username: {userName}</Text>
+
       <View style={styles.buttonContainer}>
         <Button title="Log Out" onPress={() => logOut()} />
       </View>

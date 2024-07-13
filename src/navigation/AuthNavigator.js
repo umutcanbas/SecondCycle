d25648c74c9e909dcca5920e47815cb72312e49d
umutcanbas <umutbas57@gmail.com>
2024-07-13 23:01:13 +0300

    Info page added

--- src/navigation/AuthNavigator.js
+++ src/navigation/AuthNavigator.js
@@ -5,6 +5,7 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import SplashScreen from '../screens/Auth/SplashScreen';
 import Login from '../screens/Auth/Login';
 import SingUp from '../screens/Auth/SignUp';
+import Info from '../screens/Auth/Info';
 
 import routes from './routes';
 
@@ -19,6 +20,7 @@ const AuthNavigator = () => {
       <Stack.Screen name={routes.SPLASHSCREEN} component={SplashScreen} />
       <Stack.Screen name={routes.LOGIN} component={Login} />
       <Stack.Screen name={routes.SINGUP} component={SingUp} />
+      <Stack.Screen name={routes.INFO} component={Info} />
     </Stack.Navigator>
   );
 };

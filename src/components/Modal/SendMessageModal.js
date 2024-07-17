deae153e0848468d8ecaf0df9841d2be8de2abc1
umutcanbas <umutbas57@gmail.com>
2024-07-17 15:19:45 +0300

    Send mesage modal

---
+++ src/components/Modal/SendMessageModal.js
@@ -0,0 +1,52 @@
+import {View, Dimensions, StyleSheet} from 'react-native';
+import React from 'react';
+import Modal from 'react-native-modal';
+
+import SendMessage from '../SendMessage';
+
+const deviceSize = Dimensions.get('window');
+
+const SendMessageModal = ({visible, onClose}) => {
+    
+  return (
+    <Modal
+      style={styles.modal}
+      isVisible={visible}
+      swipeDirection="down"
+      onSwipeComplete={onClose}
+      onBackdropPress={onClose}
+      onBackButtonPress={onClose}>
+      <View style={styles.container}>
+        <View style={styles.line} />
+        <View style={styles.input_container}>
+          <SendMessage onClose={onClose} />
+        </View>
+      </View>
+    </Modal>
+  );
+};
+
+const styles = StyleSheet.create({
+    container: {
+      backgroundColor: 'white',
+      padding: 15,
+      borderRadius: 10,
+      height: deviceSize.height / 1.6,
+    },
+    line: {
+      width: 80,
+      height: 8,
+      backgroundColor: '#e0e0e0',
+      borderRadius: 10,
+      alignSelf: 'center',
+      marginBottom: 5,
+    },
+    input_container: {flex: 1},
+    modal: {
+      justifyContent: 'flex-end',
+      margin: 0,
+    },
+  });
+  
+  export default SendMessageModal;
+  
